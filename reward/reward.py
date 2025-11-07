050e28c126915a12e2c78d7b68e535ac830734f5
Aqilah Raihana <aqilahraihana9@gmail.com>
2025-11-07 11:09:32 +0800

    Menambah fitur reward

---
+++ reward/reward.py
@@ -0,0 +1,30 @@
+def cek_tingkat(total_level):
+    if 0 < total_level < 10:
+        tingkat = "Belum ada tingkat yang dicapai"
+    elif 10 <= total_level < 20:
+        tingkat = "Tingkat 1 = Bronze"
+    elif 20 <= total_level < 30:
+        tingkat = "Tingkat 2 = Silver"
+    elif 30 <= total_level < 40:
+        tingkat = "Tingkat 3 = Gold"
+    elif 40 <= total_level < 50:
+        tingkat = "Tingkat 4 = Diamond"
+    elif 50 <= total_level < 60:
+        tingkat = "Tingkat 5 = Master"
+    elif total_level >= 60:
+        tingkat = "Tingkat 6 = Legendary"
+    else:
+        tingkat = None
+    
+    return tingkat
+
+def proses_level(kategori, level_dikerjakan, total_level):
+    print(f"Kategori: {kategori}, Level ke-{level_dikerjakan} selesai.")
+    print(f"Total level yang sudah dikerjakan: {total_level}")
+    
+    tingkat = cek_tingkat(total_level)
+    
+    if total_level % 10 == 0:
+        print(f"🎉 Selamat! Anda naik ke {tingkat}")
+    else:
+        print(f"Anda berada di {tingkat}.")
